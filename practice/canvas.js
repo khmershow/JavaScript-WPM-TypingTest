1359f1007b4fd6df10f325b367bb39268136ca16
David Steeby <davidsteeby@gmail.com>
2012-08-16 09:05:36 -0600

    restructured folder

---
+++ practice/canvas.js
@@ -0,0 +1,346 @@
+// JavaScript Document
+
+var canvas, ctx;
+var window_Height, window_Width;
+
+var imageCount = 0;
+
+var  startFinishImg, roadImg, playerCarImg, opponentCarImg;
+
+var startFinishImgPath = "Images/startFinish.png";
+var roadImgPath = "Images/road.png";
+var playerCarPath = "Images/car1.png";
+var oppenentCarPath = "Images/car2.png";
+
+var roadImgRatio, carImgRatio;
+
+var roadTimer, carTimer;
+
+var startingLine = true;
+var roadOffset = .5;
+
+var playerRand = 0, opponentRand = 0;
+var switchRand = true, moveStart = true, finishLine = false, enterFinish1 = false, enterFinish2 = false;
+var carOffset = oppOffset = .08;
+var destination;
+
+var tInput, eInput, aInput, WPM;
+var WPMCount, tCount, eCount, aCount;
+
+/******testing******/
+var finishEl;
+var fadeCanvas = false, alpha = 1;
+
+function init () {
+	
+	startFinishImg = new Image();
+	eventRegistrar(startFinishImg,'load',onImage);
+	startFinishImg.src = startFinishImgPath;
+	
+	roadImg = new Image();
+	eventRegistrar(roadImg, 'load',onImage,false);
+	roadImg.src = roadImgPath;
+	
+	playerCarImg = new Image();
+	eventRegistrar(playerCarImg, 'load',onImage);
+	playerCarImg.src = playerCarPath;
+	
+	opponentCarImg = new Image();
+	opponentCarImg.src = oppenentCarPath;
+	eventRegistrar(opponentCarImg, 'load',onImage);
+	
+}
+
+/********************************
+*
+*onImage allows all for images to load before proceeding
+*
+*if the number of images increase, the number in the if(imageCount == *number*) needs to be increased
+********************************/
+function onImage () {	
+	imageCount++;
+	if(imageCount == 4)
+		setup();
+}
+
+/********************************
+*
+*This function "setup" is the first of the functions needed to setup the canvas
+*
+*gets window size, retrieves elements into javascrip variables and sets event for changin WPM
+*
+*calls setCanvas() and initDrawing() at the end
+********************************/
+function setup () {
+	var size;
+	
+	size = get_window_size();
+	
+	window_Height = size[0];
+	window_Width = size[1];
+	
+	roadImgRatio = roadImg.height/roadImg.width;
+	carImgRatio = playerCarImg.width/playerCarImg.height;
+	
+	tInput = document.getElementById("terribleWPM");
+	tCount = 1 * tInput.value;
+	
+	eInput = document.getElementById("expectedWPM");
+	eCount = 1 * eInput.value;
+	
+	aInput = document.getElementById("awesomeWPM");
+	aCount = 1 * aInput.value;
+	
+	WPM = document.getElementById("testWPM");
+	WPMCount = 1 * WPM.value;
+	
+	finishEl = document.getElementById("fButton");
+	eventRegistrar(finishEl, 'click', setFinish);	
+		
+		
+	eventRegistrar(WPM, 'change', setWPMVariable);
+		
+	setCanvas();
+	initDrawing();
+}
+
+function setFinish(e){
+	finishLine = !finishLine;
+}
+
+
+/*************************************
+*
+*attached via a listener to WPM. Whenever the WPM changes, the javascript value is updated
+*
+*************************************/
+function setWPMVariable(e) {
+		var theEl = getTarget(e);
+		WPMCount = 1 * theEl.value;	
+}
+
+/*************************************
+*
+*returns object to which the event (e) just triggered on
+*
+*************************************/
+function getTarget(e){
+	
+	var targetElement;
+	if (!e) var e = window.event;
+	if (e.target) targetElement = e.target;
+	else if (e.srcElement) targetElement = e.srcElement;
+	if (targetElement.nodeType == 3) // defeat Safari bug
+		targetElement = targetElement.parentNode;
+	return targetElement;
+}
+
+/*************************************
+*
+*universal event registration, cross compatible
+*
+*************************************/
+function eventRegistrar(element, eventType, handlerFunction) {
+	if(element.addEventListener) {
+		element.addEventListener(eventType,handlerFunction,false);
+	} else {
+		element.attachEvent('on'+eventType,handlerFunction);
+	}
+}
+
+/*************************************
+*
+*retrieves canvas element and context and sets hieght and width
+*
+*************************************/
+function setCanvas () { 
+	var typingDivHeight;
+
+	typingDivHeight = document.getElementById("typingDiv").offsetHeight;
+			
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext('2d');
+
+	if(window_Width > 1000)
+		canvas.width = 1000;
+	else
+		canvas.width = window_Width*.95;
+	
+	canvas.height = canvas.width * roadImgRatio;
+}
+
+/*************************************
+*
+*sets initial drawing, simple still image
+*
+*************************************/
+function initDrawing () {
+	ctx.drawImage(startFinishImg, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(opponentCarImg, 0, canvas.height * .25, canvas.height * .5 * carImgRatio, canvas.height*.5);
+	ctx.drawImage(playerCarImg, 0, canvas.height * .35, canvas.height * .5 * carImgRatio, canvas.height*.5);
+}
+
+/*************************************
+*
+*disables start button and starts intervals
+*
+*************************************/
+function startRace () {
+	var startButton = document.getElementById("startButton");
+	startButton.setAttribute('disabled','disabled');
+	
+	
+	roadTimer = setInterval("moveRoad()", 16);
+	carTimer = setInterval("moveCar()", 16);
+}
+
+/*************************************
+*
+*
+*handles the movement of the road
+*starts with start line and then moves to the generic road
+*
+*
+*************************************/
+function moveRoad () {
+	var roadWidthRatio = startFinishImg.width/canvas.width;
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	if(roadOffset < canvas.width){
+		if(startingLine)
+			roadOffset *= 1.02;
+		else			
+			roadOffset += 20;
+	}else{ 
+		roadOffset = 0;
+		startingLine = false;
+		if(finishLine)
+			enterFinish1 =true;
+	}
+
+	
+	if(startingLine){
+		ctx.drawImage(startFinishImg, roadOffset * roadWidthRatio, 0, startFinishImg.width - roadOffset*roadWidthRatio, startFinishImg.height, 0, 0, canvas.width - roadOffset, canvas.height);
+		ctx.drawImage(roadImg, 0, 0, roadOffset * roadWidthRatio, startFinishImg.height, canvas.width - roadOffset, 0, roadOffset, canvas.height);
+	}else if(enterFinish1){
+		
+		ctx.drawImage(roadImg, roadOffset * roadWidthRatio, 0, startFinishImg.width - roadOffset*roadWidthRatio, startFinishImg.height, 0, 0, canvas.width - roadOffset, canvas.height);
+		ctx.drawImage(startFinishImg, 0, 0, roadOffset * roadWidthRatio, startFinishImg.height, canvas.width - roadOffset, 0, roadOffset, canvas.height);
+		if(roadOffset  >= canvas.width){
+			enterFinish1 = false;
+			finishLine = false;
+			enterFinish2 = true;
+		}
+	}else if(enterFinish2){
+		ctx.drawImage(startFinishImg, roadOffset * roadWidthRatio, 0, startFinishImg.width - roadOffset*roadWidthRatio, startFinishImg.height, 0, 0, canvas.width - roadOffset, canvas.height);
+		ctx.drawImage(roadImg, 0, 0, roadOffset * roadWidthRatio, startFinishImg.height, canvas.width - roadOffset, 0, roadOffset, canvas.height);
+		if(roadOffset  >= canvas.width){
+			enterFinish2 = false;
+			WPM.removeEventListener('change',setWPMVariable,false);
+			fadeCanvas = true;
+		}
+	}else{
+		if(fadeCanvas){
+			alpha -= .005;
+			if(alpha < .001){
+				endOfGame();
+				return;
+			}
+			ctx.globalAlpha = alpha;
+		}
+		ctx.drawImage(roadImg, roadOffset * roadWidthRatio, 0, roadImg.width - roadOffset*roadWidthRatio, roadImg.height, 0, 0, canvas.width - roadOffset, canvas.height);
+		ctx.drawImage(roadImg, 0, 0, roadOffset * roadWidthRatio, startFinishImg.height, canvas.width - roadOffset, 0, roadOffset, canvas.height);
+	}
+}
+
+function endOfGame() {
+	clearInterval(roadTimer);
+	clearInterval(carTimer);
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.globalAlpha = 1;
+	ctx.font        = "normal 36px Arial";
+	ctx.strokeStyle = "#000000";
+	ctx.strokeText("Congratulations!!!!", 50, 90);
+}
+
+/*************************************
+*
+*
+*handles the movement of the vehicles, both forward and backwards as well as up and down (creates "rumble" of engines)
+*
+*
+*************************************/
+function moveCar () {
+	var roadWidthRatio = playerCarImg.width/canvas.width;
+	var playerPosition = carOffset;
+	var opponentPosition = oppOffset;
+	
+	/*****this is what does the up and down "rumble"*****/
+	if(!switchRand){
+		playerRand = -Math.floor(Math.random()+1);
+		opponentRand = Math.floor(Math.random()+1);
+		switchRand = true;
+	}else{
+		playerRand *= -1;
+		opponentRand *= -1;
+		switchRand = false;
+	}
+	
+	if(carOffset >(canvas.width*.5 - (canvas.height *.25 * carImgRatio))){
+		moveStart= false;
+	}
+	
+	if(moveStart)
+	
+		/*****creates acceleration*****/
+		
+		oppOffset = carOffset *= 1.015;
+	else{
+		
+		/*****movement backward and forward based on updated WPMCount*****/
+		
+		if(WPMCount >= tCount && WPMCount <= aCount)
+			destination = canvas.width - ((WPMCount-tCount)/(aCount-tCount))*canvas.width - (canvas.height *.25 * carImgRatio);
+		else if(WPMCount < tCount)
+			destination = canvas.width - (canvas.height *.25 * carImgRatio);
+		else if (WPMCount > aCount)
+			destination = (-1*(canvas.height *.25 * carImgRatio));
+		
+		if(destination > oppOffset)
+			oppOffset++;
+		else if(destination < oppOffset)
+			oppOffset--;
+	}
+		
+	
+	/*****acrual drawing of the cars*****/
+	ctx.drawImage(opponentCarImg, oppOffset, canvas.height * .25 + opponentRand, canvas.height * .5 * carImgRatio, canvas.height*.5);
+	ctx.drawImage(playerCarImg, carOffset, canvas.height * .35 + playerRand, canvas.height * .5 * carImgRatio, canvas.height*.5);
+	
+}
+
+/***** 
+*
+* gets windows size, works with all browsers
+*
+*****/
+function get_window_size () {
+	var winW = 630, winH = 460;
+	if (document.body && document.body.offsetWidth) {
+		winW = document.body.offsetWidth;
+		winH = document.body.offsetHeight;
+	}
+	if (document.compatMode=='CSS1Compat' &&
+		document.documentElement &&
+		document.documentElement.offsetWidth ) {
+		winW = document.documentElement.offsetWidth;
+		winH = document.documentElement.offsetHeight;
+	}
+	if (window.innerWidth && window.innerHeight) {
+		winW = window.innerWidth;
+		winH = window.innerHeight;
+	} 	
+	return [ winH, winW];
+}
---
+++ practice/practiceTest.php
@@ -0,0 +1,132 @@
+<?PHP
+	$error=0;
+	function GetError($str1,$str2){
+		$error=0;
+		for($i=0;$i<strlen($str1);$i++){
+			if(isset($str2[$i])){
+				if($str1[$i] !=$str2[$i])
+				$error++;
+				} else
+			$error++;
+		}
+		return $error;
+	}
+	$totalError=0;
+	$total_time=0;
+	$char=0;
+	$wpm="Not value";
+	$cpm="Not value";
+	$accuracy="Not value";
+	$readonly='readonly="readonly"';
+	$welcome='Press button "Start Test"';
+	$les_text='Type the text that appears here in the box below.';
+	$cor_text=file_get_contents( "typingtext/0/0.txt" );
+	$time_start=0;
+	$user_text="";
+	$start_time=0;
+	if(isset($_POST["startTime"])){
+		$start_time=$_POST["startTime"];
+	}
+	if(isset($_POST["vstart"])){
+		$time_start=microtime(true);
+		$readonly="";
+		$welcome="";
+		$les_text=file_get_contents( "typingtext/0/0.txt" );
+	}
+	if(isset($_POST["done"])){
+		$les_text=file_get_contents( "typingtext/0/0.txt" );
+		$user_text=str_replace("\r\n","",$_POST["user_text"]); 
+		$user_text=str_replace("\n","",$user_text);
+		$total_time=microtime(true)-$start_time;
+		$char=strlen($les_text);
+		$word=substr_count($les_text,' ') + 1;
+		$wpm=round(($char/5)/($total_time / 60));
+		$cpm=round((($char/5)-$totalError)/($total_time / 60));
+		$totalError = (GetError($les_text,$user_text));
+		$totalWords = ($word);
+		$accuracy=100-round(GetError($les_text,$user_text) * 100 /$char);
+		$readonly='readonly="readonly"';			
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Typing Test Alpha 0.0.1</title> 
+    <script type="text/javascript" src="timer.js"></script>
+    <script type="text/javascript" src="checker.js"></script>
+    <script type="text/javascript" src="copyPaste.js"></script>
+    <script type="text/javascript" src="htmlOverlay.js"></script>
+	<script type="text/javascript"> 
+		var startClicked = <?PHP echo (isset($_POST['vstart']))?"true":"false"; ?>;
+		if (startClicked==true){
+			InitTimer();
+			textCorrect();
+		}
+		
+		var message="Sorry, right-click has been disabled"; 
+		function clickIE() {if (document.all) {(message);return false;}} 
+		function clickNS(e) {if 
+		(document.layers||(document.getElementById&&!document.all)) { 
+		if (e.which==2||e.which==3) {(message);return false;}}} 
+		if (document.layers) 
+		{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;} 
+		else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;} 
+		document.oncontextmenu=new Function("return false") 
+	</script> 
+</head>
+<body onLoad="">
+    <div class="ac">
+        <form method='post' action="test.php">
+        <br />	
+        	
+             <?php if(isset($_POST["done"])){ ?> 
+          						
+                <table width="449" cellpadding="6" cellspacing="0" class="ta">
+                    <tr>
+                        <th width="162" class="th" style="width: 10%"><div align="left">Parameter</div></th>
+                        <th width="131" class="th" style="width: 10%"><div align="left">Your result</div></th>
+                    </tr>
+                    <tr> 
+                        <td class="td"><b>Total Words</b> (#)</td> 
+                        <td class="td"><b><?PHP echo $totalWords?></b></td>
+                  </tr>
+                        <tr>
+                        <td class="td"><b>GWPM</b> (gross word per minutes)</td>
+                        <td class="td"><b><?PHP echo $wpm?></b></td>
+                        </tr>
+                        <tr> 
+                        <td class="td"><b>Errors</b> (#)</td> 
+                        <td class="td"><b><?PHP echo $totalError?></b></td>
+                        </tr>
+                        <tr>
+                        <td class="td"><b>CWPM</b> (correct words per minutes)</td> 
+                        <td class="td"><b><?PHP echo $cpm?></b></td>
+                        </tr>
+                  <tr>
+                        <td class="td"><b>Accuracy</b> (%)</td> 
+                        <td class="td"><b><?PHP echo $accuracy?></b></td>
+                        </tr>
+                        
+                    <tr></tr>
+                      <td>&nbsp;</td>
+              </table>
+			              
+          <?PHP }else{?>
+           	    Time remaining:<input id="txt" readonly type="text" value="" border="0" name="disp">
+            	<br />
+                <textarea id="area1"  onkeydown="return disableCtrlKeyCombination(event);" onKeyUp="return disableCtrlKeyCombination(event);" readonly rows="5" cols="72"><?PHP echo $les_text?></textarea>	
+                <br /> 
+                <input class="in" type="submit" name="vstart" id="vStart" value="Start Test"/>
+                <br /> 
+                <textarea id="userText" onKeyDown="return disableCtrlKeyCombination(event); " onKeyUp="diffString1(document.getElementById('area1').value,this.value);"  rows="5" cols="72" name="user_text" ><?PHP echo $welcome; ?></textarea>
+                <div id="textCorrection" style="position: absolute; z-index:1; left: 10px; top: 10px; width:200px; height:100px;">
+    			</div>
+                <br />  
+                <input type="submit" name="done" id="done" value="Done" />
+                <input type="hidden" name="startTime" value="<?PHP echo $time_start ?>" />
+           <?PHP }?>
+      </form>
+    </div>
+</body>
+</html>
+ 
