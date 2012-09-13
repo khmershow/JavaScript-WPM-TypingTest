71ebd141b3c220cabbb3843e545bd0dcd341df28
dsteeby <davidsteeby@gmail.com>
2012-09-13 16:57:35 -0600

    jquery additions

--- final/WPM.js
+++ final/WPM.js
@@ -3,11 +3,25 @@ start button functionality
 all variables
 */
 var finalText = "This is the final.";
+var totalWords = null;
+var wpm = null;
+var totalError = null;
+var cpm = null;
+var accuracy = null;
 
 function initText(){
 	$("#area1").html(finalText);
 	
 }
-function addTableData(){
+
+function grader(){
 	
+}
+
+function addTableData(){
+	$("#totalWords").html(totalWords);
+	$("#wpm").html(wpm);
+	$("#totalError").html(totalError);
+	$("#cpm").html(cpm);
+	$("#accuracy").html(accuracy);
 }
