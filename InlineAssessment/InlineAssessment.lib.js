af822022787e7baf9f5fa3a8e14d2b6e0d01899f
dsteeby <davidsteeby@gmail.com>
2012-10-16 10:45:52 -0600

    added menu

--- InlineAssessment/InlineAssessment.lib.js
+++ InlineAssessment/InlineAssessment.lib.js
@@ -30,6 +30,18 @@ function InlineAssessment(elementArg) {
 		},
 		'simple_text': {
 				'inputElementsString':"<input/><input type=\"button\" value=\"submit thingie\"/>"
+		},
+		'simple_menu': {
+				'inputElementString':"<select>" + 
+					"	<option  value=\"100\">100%</ option>" +
+					"	<option  value=\"90\">90%</ option>" +
+					"	<option  value=\"80\">80%</ option>" +
+					"	<option  value=\"70\">70%</ option>" +
+					"	<option  value=\"60\">60%</ option>" +
+					"	<option  value=\"50\">50%</ option>" +
+					"	<option  value=\"10\">10%</ option>" +
+					"</select>" +
+					"<input type=\"submit\" value=\"Submit\" onClick=\"setScore(value)\">"	
 		}
 	};
 	
