05e19466ee08283a6815b3f29e30a03aea741f5c
dsteeby <davidsteeby@gmail.com>
2012-10-23 17:16:55 -0600

    fixed the portal. All working now.

--- final/portal.php
+++ final/portal.php
@@ -1,10 +1,12 @@
-<?php /*?><?PHP
+<?PHP
 // This file is meant to be called using Ajax from anywhere. It adds lines to a log file, so some injection attacks should be cleaned out...
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
 header("Access-Control-Allow-Headers: *");
 ?>
 <?php 
+
+$return_json = "{\"file_lock\":";
 
 	$bytes_written = 0;										//	Initialize our file write counter
 	
@@ -14,23 +16,23 @@ header("Access-Control-Allow-Headers: *");
 	$fh = fopen($myFile, 'w');								//	Open the log for writing! (will overwrite previous information in file)
 	if(flock($fh, LOCK_EX)) {								//	Lock the file to force exclusive write
 		
-		$stringData =  $_POST["JSONString"];				//	get the JSON string and write it to the file
+		$stringData = 1;									//	get the JSON string and write it to the file
 		$bytes_written += fwrite($fh, $stringData);
 		
 		flock($fh, LOCK_UN); 								//	Now that we've written all the data we can unlock the file!
 		fclose($fh);										//	... and close it.
 		
 		if($bytes_written > 0)								//	Now to let the client know what happened.
-			echo '{"status":"success","bytes_written":"'.$bytes_written.'"}';
+			$return_json .= '{"status":"success","bytes_written":"'.$bytes_written.'"}';
 		else
-			echo '{"status":"failure", "message":"Error:write failed!"}';
+			$return_json .= '{"status":"failure", "message":"Error:write failed!"}';
 	} else {
-		echo '{"status":"failure","message":"Error:file lock failed!"}';
+		$return_json .= '{"status":"failure","message":"Error:file lock failed!"}';
 	}
-?><?php */?>
+?>
 <?PHP
 session_start();
-$return_json = "{";
+$return_json .= ",";
 /*Define variables*/
 	$readonly='readonly="readonly"';
 	$les_text=file_get_contents( "typingtext/0/0.txt" );
